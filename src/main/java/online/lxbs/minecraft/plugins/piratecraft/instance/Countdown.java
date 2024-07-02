78318228b36480d535095154f299365481530684
wyatt_was <wyatt.ruehl@mailbox.org>
2024-07-02 18:04:57 +0200

    create new countdown task

---
+++ src/main/java/online/lxbs/minecraft/plugins/piratecraft/instance/Countdown.java
@@ -0,0 +1,39 @@
+package online.lxbs.minecraft.plugins.piratecraft.instance;
+
+import online.lxbs.minecraft.plugins.piratecraft.GameState;
+import online.lxbs.minecraft.plugins.piratecraft.Piratecraft;
+import online.lxbs.minecraft.plugins.piratecraft.managers.ConfigManager;
+import org.bukkit.ChatColor;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class Countdown extends BukkitRunnable {
+    private Piratecraft piratecraft;
+    private Arena arena;
+    private int countdownSeconds;
+
+    public Countdown(Piratecraft piratecraft, Arena arena) {
+        this.piratecraft = piratecraft;
+        this.arena = arena;
+        this.countdownSeconds = ConfigManager.getCountdownSeconds();
+    }
+
+    public void start() {
+        arena.setState(GameState.COUNTDOWN);
+        runTaskTimer(piratecraft, 0, 20);
+    }
+
+    @Override
+    public void run() {
+        if (countdownSeconds == 0) {
+            cancel();
+            arena.start();
+            return;
+        }
+
+        if (countdownSeconds <= 10 || countdownSeconds % 15 == 0) {
+            arena.sendMessage(ChatColor.GREEN + "Game will start in " + countdownSeconds + " second" + (countdownSeconds == 1 ? "" : "s") + ".");
+        }
+
+        countdownSeconds--;
+    }
+}
